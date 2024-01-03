b83def4125684a988bd93f5d14ba75e7d2446ab7
karenhernandez <khernandez0329@gmail.com>
2024-01-02 17:06:15 -0800

    added trading

--- lib/models/Enemy.py
+++ lib/models/Enemy.py
@@ -1,57 +1,62 @@
 from models.__init__ import CURSOR, CONN
-# import random
+
+import random
+
 
 class Enemy:
     all = {}
-    
+
     def __init__(self, type, hp, damage, name=None, id=None):
         self.id = id
         self.name = name
         self.type = type
         self.hp = hp
         self.damage = damage
-        
+
     @property
     def name(self):
         return self._name
+
     @name.setter
     def name(self, name):
         if isinstance(name, str):
             self._name = name
         else:
             raise ValueError("Name must be a string.")
-        
+
     @property
     def type(self):
         return self._type
+
     @type.setter
     def type(self, type):
         if isinstance(type, str):
             self._type = type
         else:
             raise ValueError("Type must be a string.")
-        
+
     @property
     def hp(self):
         return self._hp
+
     @hp.setter
     def hp(self, hp):
         if isinstance(hp, int) and hp >= 0:
             self._hp = hp
         else:
             raise ValueError("HP must be a positive integer.")
-        
+
     @property
     def damage(self):
         return self._damage
+
     @damage.setter
     def hp(self, damage):
         if isinstance(damage, int) and damage >= 0:
             self._damage = damage
         else:
             raise ValueError("Damage must be a positive integer.")
-        
-        
+
     @classmethod
     def create_table(cls):
         sql = """ CREATE TABLE IF NOT EXISTS enemies (
@@ -63,21 +68,17 @@ class Enemy:
               """
         CURSOR.execute(sql)
         CONN.commit()
-    
+
     @classmethod
     def drop_table(cls):
         sql = """DROP TABLE IF EXISTS enemies"""
         CURSOR.execute(sql)
         CONN.commit()
 
-    
     def save(self):
         sql = """INSERT INTO enemies (name, type, hp, damage)
                  VALUES (?, ?, ?, ?)"""
-        CURSOR.execute(sql, (
-                            self.name, self.type, self.hp, self.damage
-                            )
-                        )
+        CURSOR.execute(sql, (self.name, self.type, self.hp, self.damage))
         CONN.commit()
         self.id = CURSOR.lastrowid
         type(self).ALL[self.id] = self
@@ -86,10 +87,7 @@ class Enemy:
         sql = """UPDATE enemies
                  SET name = ?, type = ?, hp = ?, damage = ?
                  WHERE id = ?"""
-        CURSOR.execute(sql, (
-                            self.name, self.type, self.hp, 
-                            self.damage, self.id)
-                        )
+        CURSOR.execute(sql, (self.name, self.type, self.hp, self.damage, self.id))
         CONN.commit()
 
     def delete(self):
@@ -106,7 +104,7 @@ class Enemy:
             enemy.name = row[1]
             enemy.type = row[2]
             enemy.hp = row[3]
-            enemy.damage = row[4]           
+            enemy.damage = row[4]
         else:
             enemy = cls(row[1], row[2], row[3], row[4])
             enemy.id = row[0]
@@ -126,25 +124,24 @@ class Enemy:
         return cls.instance_from_db(row) if row else None
 
 
-    
-    
-# class GrimReaper(Enemy):
-#     def __init__(self, room):
-#         self.name = "The Grim Reaper"
-#         self.type = "boss"
-#         self.hp = 50
-#         self.damage = random.randint(10, 20)
-#         self.room = room
-        
-# class BlackCat(Enemy):
-#     def __init__(self):
-#         self.name = "Black Cat"
-#         self.hp = 9
-#         self.damage = 2
-        
-# class Poltergeist(Enemy):
-#     def __init__(self):
-#         self.name = "Ghost"
-#         self.hp = 10
-#         self.damage = random.randint(5, 10)
-        
+class GrimReaper(Enemy):
+    def __init__(self, room):
+        self.name = "The Grim Reaper"
+        self.type = "boss"
+        self.hp = 50
+        self.damage = random.randint(10, 20)
+        self.room = room
+
+
+class BlackCat(Enemy):
+    def __init__(self):
+        self.name = "Black Cat"
+        self.hp = 9
+        self.damage = 2
+
+
+class Poltergeist(Enemy):
+    def __init__(self):
+        self.name = "Ghost"
+        self.hp = 10
+        self.damage = random.randint(5, 10)
--- lib/models/rooms.py
+++ lib/models/rooms.py
@@ -1,4 +1,6 @@
 import random
+import Enemy
+import NonPlayChar
 
 
 class MapRoom:
@@ -50,12 +52,12 @@ class EnemyAndFriends(MapRoom):
     def __init__(self, x, y):
         r = random.random()
         if r < 0.50:
-            # self.enemy = enemies.BlackCat()
+            self.enemy = Enemy.BlackCat()
             self.alive_text = ""
             self.dead_text = ""
 
         elif r < 0.75:
-            # self.enemy = enemies.?()
+            self.enemy = Enemy.Poltergeist()
             self.alive_text = ""
             self.dead_text = ""
 
@@ -65,26 +67,66 @@ class EnemyAndFriends(MapRoom):
             self.dead_text = ""
 
         else:
-            # self.enemy = enemies.GrimReaper()
+            self.enemy = Enemy.GrimReaper()
             self.alive_text = ""
             self.dead_text = ""
 
         super().__init__(x, y)
 
-    def intro_text(self):
-        text = self.alive_text if self.enemy.alive() else self.dead_text
-        return text
+    # def intro_text(self):
+    #     text = self.alive_text if self.enemy.alive() else self.dead_text
+    #     return text
 
-    def modify_player(self, player):
-        if self.enemy.alive():
-            player.hp = player.hp - self.enemy.damage
-            print("".format(self.enemy.damage, player.hp))
+    # def modify_player(self, player):
+    #     if self.enemy.alive():
+    #         player.hp = player.hp - self.enemy.damage
+    #         print(
+    #             "Enemy does {} damage. You have {} HP remaining.".format(
+    #                 self.enemy.damage, player.hp
+    #             )
+    #         )
 
 
 class TradingGhost(MapRoom):
     def intro_text(self):
         return """You see something lurking, I think it wants to trade???"""
 
-    # def __init__(self, x, y):
-    #     self.trader
-    #     super().__init__(x, y)
+    def __init__(self, x, y):
+        self.trader = NonPlayChar.Casper()
+        super().__init__(x, y)
+
+    def trade(self, consumer, seller):
+        for i, item in enumerate(seller.inventory, 1):
+            print("{}. {} - {} HP".format(i, item.name, item.healing_value))
+        while True:
+            user_input = input("Select your item or press q to exit")
+            if user_input in ["q"]:
+                return
+            else:
+                try:
+                    choice = int(user_input)
+                    exchange = seller.inventory[choice - 1]
+                    self.transaction(seller, consumer, exchange)
+                except ValueError:
+                    print("Unacceptable selection!")
+
+    def transaction(self, seller, consumer, item):
+        if item.healing_value > consumer.hp:
+            print("That price is beyond your budget!")
+            return
+        seller.inventory.remove(item)
+        consumer.inventory.append(item)
+        seller.hp = seller.hp + item.healing_value
+        consumer.hp = seller.hp - item.healing_value
+        print("Trade sealed in ethereal terms.")
+
+    def trading(self, player):
+        print("Would you like to test your fate? (t)rade or (q)uit")
+        user_input = input()
+        if user_input in ["q"]:
+            return
+        elif user_input in ["t"]:
+            print("Behold, these are the offerings for trade from beyond the veil.")
+            self.trade(consumer=player, seller=self.trader)
+        else:
+            print("Unacceptable selection!")
