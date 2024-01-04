35e85aef89d7d7a837717bf45d9bcfb4c332d0d2
karenhernandez <khernandez0329@gmail.com>
2024-01-03 17:02:59 -0800

    fixed minor errors

--- lib/models/game.py
+++ lib/models/game.py
@@ -155,9 +155,9 @@ class Game:
         if encounter_chance < 0.9:
             self.random_encounter()
 
-            if self.current_enemy:
-                print(f"A {self.current_enemy} appeared!")
-                self.battle()
+        if self.current_enemy:
+            print(f"A {self.current_enemy} appeared!")
+            self.battle()
         print()
         output_slow(room.intro_text())
         print()
@@ -207,6 +207,9 @@ class Game:
             pass
         if choice == "2":
             Game.return_staircase(self)
+
+    def go_first_floor(self):
+        room = FirstFloorRoom()
         encounter_chance = random.random()
         if encounter_chance < 1.0:
             print(f"A {self.current_enemy} appeared!")
@@ -214,9 +217,6 @@ class Game:
 
         if self.current_enemy:
             self.battle()
-
-    def go_first_floor(self):
-        room = FirstFloorRoom()
         print()
         output_slow(room.intro_text())
         print()
@@ -265,8 +265,6 @@ class Game:
         if not self.player or not self.current_enemy:
             print("Must be alive to battle")
             return
-
-        print(f"A {self.current_enemy} appeared!")
 
         while self.player.hp > 0 and self.current_enemy.hp > 0:
             print(f"{self.player.name}'s HP: {self.player.hp}")
